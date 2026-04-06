01c5d5dc7071136365aa6b97b48adee1e674d09c
salimov sardor <salimovone@gmail.com>
2026-04-06 15:30:42 +0500

    "barchasi" is hidden

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -73,7 +73,7 @@ const DesktopNav = ({categories, subcategories = []}) => {
 							);
 						})}
 
-						{hasMoreCategories && (
+						{/* {hasMoreCategories && (
 							<Link
 								to="/categories"
 								className="px-4 py-2 mt-1 text-sm font-semibold text-blue-600 border-t border-gray-100 hover:bg-gray-50 transition"
@@ -84,7 +84,7 @@ const DesktopNav = ({categories, subcategories = []}) => {
 							>
 								Barchasi...
 							</Link>
-						)}
+						)} */}
 					</div>
 				)}
 			</div>
