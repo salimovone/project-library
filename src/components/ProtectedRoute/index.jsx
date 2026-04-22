e1164e0f772f3b1d5d55d56622bddbba26f8831b
salimov sardor <salimovone@gmail.com>
2026-04-22 11:44:32 +0500

    first time login page updated

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,8 +1,9 @@
-import { Navigate, Outlet } from 'react-router';
+import { Navigate, Outlet, useNavigate } from 'react-router';
 import useRole from '../../hooks/useRole';
 
 export default function ProtectedRoute({ requiredRole, strict = false }) {
   const { checkUserLevel, role } = useRole();
-  if(strict && role != requiredRole) return <Navigate to="/" replace />;
-  return checkUserLevel(requiredRole) ? <Outlet /> : <Navigate to="/" replace />;
+  const navigate = useNavigate();
+  if (strict && role != requiredRole) return navigate(-1);
+  return checkUserLevel(requiredRole) ? <Outlet /> : navigate(-1);
 }
